528daeb451d141d034f323227f33f8e6013e8b6c
atlas <a.sproson@outlook.com>
2022-10-10 14:03:38 +0100

    Refactors sound for i/い

--- src/components/StudyGuide.jsx
+++ src/components/StudyGuide.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const data = [
   { id: 0, hiragana: "あ", eng: "a", sound: "ah" },
-  { id: 1, hiragana: "い", eng: "i", sound: "iee" },
+  { id: 1, hiragana: "い", eng: "i", sound: "ee" },
   { id: 2, hiragana: "う", eng: "u", sound: "uoo" },
   { id: 3, hiragana: "え", eng: "e", sound: "eh" },
   { id: 4, hiragana: "お", eng: "o", sound: "oh" },
