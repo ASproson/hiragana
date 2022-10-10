9c56f14d3111b79f3826e3e7d00915571e2953a0
atlas <a.sproson@outlook.com>
2022-10-10 14:00:48 +0100

    Refactors data into a component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import "./App.css";
-
-const data = [
-  { id: 0, hiragana: "あ", eng: "a", sound: "ah" },
-  { id: 1, hiragana: "い", eng: "i", sound: "iee" },
-  { id: 2, hiragana: "う", eng: "u", sound: "uoo" },
-  { id: 3, hiragana: "え", eng: "e", sound: "eh" },
-  { id: 4, hiragana: "お", eng: "o", sound: "oh" },
-];
+import StudyGuide from "./components/StudyGuide";
 
 // ref link https://www.lexilogos.com/keyboard/hiragana.htm
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <div>
-        <h1 className="text-3xl mt-10 mb-10">Let's Learn Hiragana!</h1>
-        <div className="text-center text-3xl flex justify-center">
-          {data.map((val) => {
-            return (
-              <div key={val.id} className='mr-4'>
-                <p>{val.eng}</p>
-                <p>{val.sound}</p>
-                <p className='mt-2'>{val.hiragana}</p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+    <div className="text-center">
+      <h1 className="text-3xl mt-10 mb-10">Let's Learn Hiragana!</h1>
+      <StudyGuide />
     </div>
   );
 }
