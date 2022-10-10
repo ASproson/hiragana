6c0cb1ba5ce24f3a5bf30815ddcea8f3618ae9e5
atlas <a.sproson@outlook.com>
2022-10-10 14:05:41 +0100

    Updates App.js with introductory comments

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ import StudyGuide from "./components/StudyGuide";
 function App() {
   return (
     <div className="text-center">
-      <h1 className="text-3xl mt-10 mb-10">Let's Learn Hiragana!</h1>
+      <h1 className="text-3xl mt-10 mb-4">Let's Learn Hiragana!</h1>
+      <div className="mb-10">
+        <p>
+          This is resource I am building to practice Hiragana, there are very
+          likely mistakes!
+        </p>
+        <p> So please be patient and raise an issue on GitHub for any fixes</p>
+      </div>
       <StudyGuide />
     </div>
   );
