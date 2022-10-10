476c517b7815c79e3b313358ad5db725cfd0e846
atlas <a.sproson@outlook.com>
2022-10-10 18:25:58 +0100

    Refactors to remove Nav links to seperate component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 // ref link https://www.lexilogos.com/keyboard/hiragana.htm
 
 function App() {
   return (
-    <div className="text-center">
-      <h1 className="text-3xl pb-4 pt-10">Let's Learn Hiragana!</h1>
-      <div className="pb-10">
-        <p>
-          This is resource I am building to practice Hiragana, there are very
-          likely mistakes!
-        </p>
-        <p> So please be patient and raise an issue on GitHub for any fixes</p>
+    <div>
+      <div className="text-center">
+        <h1 className="text-3xl pb-4 pt-10">Let's Learn Hiragana!</h1>
+        <div className="pb-10">
+          <p>
+            This is resource I am building to practice Hiragana, there are very
+            likely mistakes!
+          </p>
+          <p>So please be patient and raise an issue on GitHub for any fixes</p>
+        </div>
       </div>
-      <Link to="/studyguide">Study Guide</Link>
     </div>
   );
 }
