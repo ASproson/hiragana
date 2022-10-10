32e611d40980a4b2f4b13915b27686ebb8a121c8
atlas <a.sproson@outlook.com>
2022-10-10 18:25:38 +0100

    Refactors to show NavBar across app

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import StudyGuide from "./components/StudyGuide";
+import SetQuiz from "./components/SetQuiz";
+import RandomQuiz from "./components/RandomQuiz";
+import NavBar from "./components/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <NavBar />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/studyguide" element={<StudyGuide />} />
+      <Route path="/quiz" element={<SetQuiz />} />
+      <Route path="/randomQuiz" element={<RandomQuiz />} />
     </Routes>
   </BrowserRouter>
 );
