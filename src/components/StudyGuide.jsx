0170cc979bef41bf19c5d5a2157604445260f1de
atlas <a.sproson@outlook.com>
2022-10-11 10:28:57 +0100

    Removes hard coded data, calls from data folder

--- src/components/StudyGuide.jsx
+++ src/components/StudyGuide.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-
-const data = [
-  { id: 0, hiragana: "あ", eng: "a", sound: "ah" },
-  { id: 1, hiragana: "い", eng: "i", sound: "ee" },
-  { id: 2, hiragana: "う", eng: "u", sound: "uoo" },
-  { id: 3, hiragana: "え", eng: "e", sound: "eh" },
-  { id: 4, hiragana: "お", eng: "o", sound: "oh" },
-];
+import { hVowels } from "../data/hiraganaList";
 
 const StudyGuide = () => {
   return (
     <div>
       <div className="text-center text-3xl flex justify-center">
-        {data.map((val) => {
+        {hVowels.map((val) => {
           return (
             <div key={val.id} className="mr-4">
               <p>{val.eng}</p>
