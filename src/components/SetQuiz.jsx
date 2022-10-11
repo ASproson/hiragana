8a67a24413d3c17212ffd6cc2e06c9c9134f7e24
atlas <a.sproson@outlook.com>
2022-10-11 10:23:59 +0100

    Extracts quiz algorithm from StudyGuide to SetQuiz component

--- src/components/SetQuiz.jsx
+++ src/components/SetQuiz.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+
+const data = [
+  { id: 0, hiragana: "あ", eng: "a", sound: "ah" },
+  { id: 1, hiragana: "い", eng: "i", sound: "ee" },
+  { id: 2, hiragana: "う", eng: "u", sound: "uoo" },
+  { id: 3, hiragana: "え", eng: "e", sound: "eh" },
+  { id: 4, hiragana: "お", eng: "o", sound: "oh" },
+];
 
 const SetQuiz = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const getUniqueRandomArrayofNumbers = (min = 0, max = data.length) => {
+    let randomNumbers = [];
+    for (let i = 0; i < 2; i++) {
+      let rand = Math.floor(Math.random() * (max - 1 - min + 1));
+      if (!randomNumbers.includes(rand)) {
+        randomNumbers.push(rand);
+      } else {
+        i--;
+      }
+    }
+    return randomNumbers;
+  };
+
+  const getRandomHiragana = () => {
+    let array = getUniqueRandomArrayofNumbers();
+    let randomHiragana = [];
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < array.length; j++) {
+        if (data[i].id === array[j]) {
+          randomHiragana.push(data[i]);
+        }
+      }
+    }
+    return randomHiragana;
+  };
+
+  getRandomHiragana();
+
+  return <div></div>;
 };
 
 export default SetQuiz;
--- src/components/StudyGuide.jsx
+++ src/components/StudyGuide.jsx
@@ -8,35 +8,6 @@ const data = [
   { id: 4, hiragana: "お", eng: "o", sound: "oh" },
 ];
 
-const getUniqueRandomArrayofNumbers = (min = 0, max = data.length) => {
-  let randomNumbers = [];
-  for (let i = 0; i < 2; i++) {
-    let rand = Math.floor(Math.random() * (max - 1 - min + 1));
-    if (!randomNumbers.includes(rand)) {
-      randomNumbers.push(rand);
-    } else {
-      i--;
-    }
-  }
-  return randomNumbers;
-};
-
-const getRandomHiragana = () => {
-  let array = getUniqueRandomArrayofNumbers();
-  let randomHiragana = [];
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      if (data[i].id === array[j]) {
-        randomHiragana.push(data[i]);
-      }
-    }
-  }
-  //   console.table(randomHiragana);
-  return randomHiragana;
-};
-
-getRandomHiragana();
-
 const StudyGuide = () => {
   return (
     <div>
